eac1973e4f3a86b62f099513554d63ccd9dfb57c
HWsunqian0027 <sunny_sunqian@163.com>
2024-03-28 14:38:54 +0800

    fix ac-diamond

--- recipes/ac-diamond/build.sh
+++ recipes/ac-diamond/build.sh
@@ -1,9 +1,17 @@
 mkdir build
 cd build
+if [ `arch` = 'aarch64' ] ; then
+cmake \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+    -DCMAKE_CXX_STANDARD=98 \
+    ..
+else
 cmake \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS} -msse4.1" \
     -DCMAKE_CXX_STANDARD=98 \
     ..
+fi
 make
 make install
